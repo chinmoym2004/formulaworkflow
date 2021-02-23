29354da2a229a5a25a565281d69703cc5e356a06
Chinmoy Maity <EXT1009@TEXILAORG.COM>
2021-02-23 09:46:19 +0530

    Removed unwanted code

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,5 @@ Route::get('/', function () {
 });
 
 
-Route::get('/gmail/auth','App\Http\Controllers\GmailController@authorization');
-Route::get('/mail','App\Http\Controllers\GmailController@callback');
-Route::get('/inbox','App\Http\Controllers\GmailController@myInbox2')->name("inbox");
-Route::get('/threads','App\Http\Controllers\GmailController@myInbox')->name("threads");
-Route::get('/readonly-inbox','App\Http\Controllers\GmailController@threadJs')->name("threads");
-
-
-
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
